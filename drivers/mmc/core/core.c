4bbd25f774f5fe4878588fd8ffd9eb693d651e60
Sujith Reddy Thumma <sthumma@codeaurora.org>
2011-02-17 21:37:48 +0530

    mmc: core: Fix race between runtime PM suspend and block requests
    There is a possible race with mmc_claim_host() in mmc_sd_suspend()/
    mmc_suspend() and mmc_claim_host() in mmc_blk_issue_rq() when runtime
    pm is enabled. Fix this by blocking processing of requests until the
    previous runtime suspend is processed and then resume as part of
    msmsdcc_enable(). Previous fix has card detection failure as a side effect
    during resume.
    
    Change-Id: I9cb31269638d9db4e630eb22b973a5335af1bda4
    Signed-off-by: Sujith Reddy Thumma <sthumma@codeaurora.org>
    [sboyd: Dropped msm driver change]
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>
    [subhashj@codeaurora.org: fixed trivial merge conflict]
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -18,6 +18,7 @@
 #include <linux/random.h>
 #include <linux/slab.h>
 #include <linux/of.h>
+#include <linux/pm.h>
 #include <linux/wakelock.h>
 #include <linux/version.h>
 
@@ -1059,6 +1060,14 @@ int __mmc_claim_host(struct mmc_host *host, atomic_t *abort)
 	might_sleep();
 
 	add_wait_queue(&host->wq, &wait);
+#ifdef CONFIG_PM_RUNTIME
+	while (mmc_dev(host)->power.runtime_status == RPM_SUSPENDING) {
+		if (host->suspend_task == current)
+			break;
+		msleep(15);
+	}
+#endif
+
 	spin_lock_irqsave(&host->lock, flags);
 	while (1) {
 		set_current_state(TASK_UNINTERRUPTIBLE);
