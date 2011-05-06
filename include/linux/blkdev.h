c0c0412b1af31efb42543c3165d9f921c85c3919
shaohua.li@intel.com <shaohua.li@intel.com>
2011-05-06 11:34:32 -0600

    block: add a non-queueable flush flag
    flush request isn't queueable in some drives. Add a flag to let driver
    notify block layer about this. We can optimize flush performance with the
    knowledge.
    
    Stable: 2.6.39 only
    
    Cc: stable@kernel.org
    Signed-off-by: Shaohua Li <shaohua.li@intel.com>
    Acked-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Jens Axboe <jaxboe@fusionio.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -482,6 +482,7 @@ struct request_queue {
 	 * for flush operations
 	 */
 	unsigned int		flush_flags;
+	unsigned int		flush_not_queueable:1;
 	struct blk_flush_queue	*fq;
 
 	struct list_head	requeue_list;
@@ -1434,7 +1435,7 @@ static inline unsigned int block_size(struct block_device *bdev)
 
 static inline bool queue_flush_queueable(struct request_queue *q)
 {
-	return !test_bit(QUEUE_FLAG_FLUSH_NQ, &q->queue_flags);
+	return !q->flush_not_queueable;
 }
 
 typedef struct {struct page *v;} Sector;
