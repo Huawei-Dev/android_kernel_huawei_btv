26e8b101a0d9434ff0e4a39d971e1bc20408f306
Steve Muckle <smuckle@codeaurora.org>
2014-04-13 14:05:30 -0700

    sched: add affinity, task load information to sched tracepoints
    Knowing the affinity mask and CPU usage of a task is helpful
    in understanding the behavior of the system. Affinity information
    has been added to the enq_deq trace event, and the migration
    tracepoint now reports the load of the task migrated.
    
    Change-Id: I29d8a610292b4dfeeb8fe16174e9d4dc196649b7
    Signed-off-by: Steve Muckle <smuckle@codeaurora.org>
    Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -865,7 +865,7 @@ static inline void enqueue_task(struct rq *rq, struct task_struct *p, int flags)
 	if (!(flags & ENQUEUE_RESTORE))
 		sched_info_queued(rq, p);
 	p->sched_class->enqueue_task(rq, p, flags);
-	trace_sched_enq_deq_task(p, 1);
+	trace_sched_enq_deq_task(p, 1, cpumask_bits(&p->cpus_allowed)[0]);
 	inc_cumulative_runnable_avg(rq, p);
 }
 
@@ -875,7 +875,7 @@ static inline void dequeue_task(struct rq *rq, struct task_struct *p, int flags)
 	if (!(flags & DEQUEUE_SAVE))
 		sched_info_dequeued(rq, p);
 	p->sched_class->dequeue_task(rq, p, flags);
-	trace_sched_enq_deq_task(p, 0);
+	trace_sched_enq_deq_task(p, 0, cpumask_bits(&p->cpus_allowed)[0]);
 	dec_cumulative_runnable_avg(rq, p);
 }
 
@@ -1722,7 +1722,7 @@ void set_task_cpu(struct task_struct *p, unsigned int new_cpu)
 #endif
 #endif
 
-	trace_sched_migrate_task(p, new_cpu);
+	trace_sched_migrate_task(p, new_cpu, pct_task_load(p));
 
 	if (task_cpu(p) != new_cpu) {
 		if (p->sched_class->migrate_task_rq)
@@ -8301,6 +8301,8 @@ void __init sched_init(void)
 {
 	int i, j;
 	unsigned long alloc_size = 0, ptr;
+
+	BUG_ON(num_possible_cpus() > BITS_PER_LONG);
 
 #ifdef CONFIG_FAIR_GROUP_SCHED
 	alloc_size += 2 * nr_cpu_ids * sizeof(void **);
