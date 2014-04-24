46889480a16de6a1c768341c44710a7e81dbf579
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2014-04-24 16:43:40 -0700

    sched: window-stats: update during migration and earlier at wakeup
    During migrations accounting needs to be done in set_task_cpu() to
    subtract the task activity from the source CPU and add it to the
    destination CPU. This accounting will require that the task's window
    based load statistics be up to date.
    
    Unfortunately, the window-based statistics cannot always be updated in
    set_task_cpu() because they are already being updated in the wakeup
    path. We cannot update the statistics solely in the wakeup path
    because not all wakeups are migrations. Those non-migrating wakeups
    will not enter set_task_cpu().
    
    To ensure the window-based stats are always updated for both wakeup
    migrations and regular migrations, they are updated earlier in the
    wakeup path, and also updated in set_task_cpu if the task is already
    runnable (this ensures it is not a wakeup migration, but a regular
    migration).
    
    Change-Id: Ib246028741d0be9bb38ce93679d6e6ba25b10756
    Signed-off-by: Steve Muckle <smuckle@codeaurora.org>
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>
    [rameezmustafa@codeaurora.org]: Port to msm-3.18]
    Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org>
    [joonwoop@codeaurora.org: fixed minor conflict in fair.c]
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1550,6 +1550,9 @@ void set_task_cpu(struct task_struct *p, unsigned int new_cpu)
 			p->sched_class->migrate_task_rq(p);
 		p->se.nr_migrations++;
 		perf_event_task_migrate(p);
+
+		if (p->state == TASK_RUNNING)
+			update_task_ravg(p, task_rq(p), 1);
 	}
 
 	__set_task_cpu(p, new_cpu);
@@ -1973,7 +1976,6 @@ ttwu_do_wakeup(struct rq *rq, struct task_struct *p, int wake_flags)
 {
 	check_preempt_curr(rq, p, wake_flags);
 
-	update_task_ravg(p, rq, 0);
 	p->state = TASK_RUNNING;
 	trace_sched_wakeup(p);
 
@@ -2183,6 +2185,7 @@ try_to_wake_up(struct task_struct *p, unsigned int state, int wake_flags)
 {
 	unsigned long flags;
 	int cpu, src_cpu, success = 0;
+	struct rq *rq;
 
 	/*
 	 * If we are going to wake up a thread waiting for CONDITION we
@@ -2193,6 +2196,7 @@ try_to_wake_up(struct task_struct *p, unsigned int state, int wake_flags)
 	smp_mb__before_spinlock();
 	raw_spin_lock_irqsave(&p->pi_lock, flags);
 	src_cpu = cpu = task_cpu(p);
+	rq = cpu_rq(src_cpu);
 
 	if (!(p->state & state))
 		goto out;
@@ -2240,6 +2244,10 @@ try_to_wake_up(struct task_struct *p, unsigned int state, int wake_flags)
 	 * their previous state and preserve Program Order.
 	 */
 	smp_rmb();
+
+	raw_spin_lock(&rq->lock);
+	update_task_ravg(p, rq, 0);
+	raw_spin_unlock(&rq->lock);
 
 	p->sched_contributes_to_load = !!task_contributes_to_load(p);
 	p->state = TASK_WAKING;
@@ -2326,8 +2334,10 @@ static void try_to_wake_up_local(struct task_struct *p)
 
 	trace_sched_waking(p);
 
-	if (!task_on_rq_queued(p))
+	if (!task_on_rq_queued(p)) {
+		update_task_ravg(p, rq, 0);
 		ttwu_activate(rq, p, ENQUEUE_WAKEUP);
+	}
 
 	ttwu_do_wakeup(rq, p, 0);
 	ttwu_stat(p, smp_processor_id(), 0);
