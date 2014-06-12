67df7779cbc020d7558ff5c87bc7f89f779edfe1
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2014-06-11 23:26:09 -0700

    sched: Add CONFIG_SCHED_HMP Kconfig option
    Add a compile-time flag to enable or disable scheduler features for
    HMP (heterogenous multi-processor) systems. Main feature deals with
    optimizing task placement for best power/performance tradeoff.
    
    Also extend features currently dependent on CONFIG_SCHED_FREQ_INPUT to
    be enabled for CONFIG_HMP as well.
    
    Change-Id: I03b3942709a80cc19f7b934a8089e1d84c14d72d
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>
    [rameezmustafa@codeaurora.org]: Port to msm-3.18]
    Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org>
    [joonwoop@codeaurora.org: fixed minor ifdefry conflict.]
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3026,7 +3026,7 @@ static inline int idle_balance(struct rq *rq)
 
 #endif /* CONFIG_SMP */
 
-#ifdef CONFIG_SCHED_FREQ_INPUT
+#if defined(CONFIG_SCHED_FREQ_INPUT) || defined(CONFIG_SCHED_HMP)
 
 static inline unsigned int task_load(struct task_struct *p)
 {
@@ -3089,7 +3089,7 @@ static inline void decay_scaled_stat(struct sched_avg *sa, u64 periods)
 			   periods);
 }
 
-#else  /* CONFIG_SCHED_FREQ_INPUT */
+#else  /* CONFIG_SCHED_FREQ_INPUT || CONFIG_SCHED_HMP */
 
 static inline void
 add_to_scaled_stat(int cpu, struct sched_avg *sa, u64 delta)
@@ -3100,7 +3100,7 @@ static inline void decay_scaled_stat(struct sched_avg *sa, u64 periods)
 {
 }
 
-#endif /* CONFIG_SCHED_FREQ_INPUT */
+#endif /* CONFIG_SCHED_FREQ_INPUT || CONFIG_SCHED_HMP */
 
 static void enqueue_sleeper(struct cfs_rq *cfs_rq, struct sched_entity *se)
 {
--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -665,7 +665,7 @@ struct rq {
 	u64 max_idle_balance_cost;
 #endif
 
-#ifdef CONFIG_SCHED_FREQ_INPUT
+#if defined(CONFIG_SCHED_FREQ_INPUT) || defined(CONFIG_SCHED_HMP)
 	/*
 	 * max_freq = user or thermal defined maximum
 	 * max_possible_freq = maximum supported by hardware
@@ -943,7 +943,7 @@ static inline void sched_ttwu_pending(void) { }
 #include "stats.h"
 #include "auto_group.h"
 
-#ifdef CONFIG_SCHED_FREQ_INPUT
+#if defined(CONFIG_SCHED_FREQ_INPUT) || defined(CONFIG_SCHED_HMP)
 
 extern unsigned int sched_ravg_window;
 extern unsigned int max_possible_freq;
@@ -964,7 +964,7 @@ dec_cumulative_runnable_avg(struct rq *rq, struct task_struct *p)
 	BUG_ON((s64)rq->cumulative_runnable_avg < 0);
 }
 
-#else	/* CONFIG_SCHED_FREQ_INPUT */
+#else	/* CONFIG_SCHED_FREQ_INPUT || CONFIG_SCHED_HMP */
 
 static inline int pct_task_load(struct task_struct *p) { return 0; }
 
@@ -980,7 +980,7 @@ dec_cumulative_runnable_avg(struct rq *rq, struct task_struct *p)
 
 static inline void init_new_task_load(struct task_struct *p) { }
 
-#endif	/* CONFIG_SCHED_FREQ_INPUT */
+#endif	/* CONFIG_SCHED_FREQ_INPUT || CONFIG_SCHED_HMP */
 
 #ifdef CONFIG_CGROUP_SCHED
 
@@ -1279,15 +1279,15 @@ struct sched_class {
 #endif
 };
 
-#ifdef CONFIG_SCHED_FREQ_INPUT
+#if defined(CONFIG_SCHED_FREQ_INPUT) || defined(CONFIG_SCHED_HMP)
 extern void
 update_task_ravg(struct task_struct *p, struct rq *rq, int update_sum);
-#else	/* CONFIG_SCHED_FREQ_INPUT */
+#else	/* CONFIG_SCHED_FREQ_INPUT || CONFIG_SCHED_HMP */
 static inline void
 update_task_ravg(struct task_struct *p, struct rq *rq, int update_sum)
 {
 }
-#endif	/* CONFIG_SCHED_FREQ_INPUT */
+#endif	/* CONFIG_SCHED_FREQ_INPUT || CONFIG_SCHED_HMP */
 
 static inline void put_prev_task(struct rq *rq, struct task_struct *prev)
 {
