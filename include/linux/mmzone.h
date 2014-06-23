3fd057663062fe12c59a473bb390f2cdec3617f3
Liam Mark <lmark@codeaurora.org>
2014-06-23 14:13:47 -0700

    mm: add cma pcp list
    Add a cma pcp list in order to increase cma memory utilization.
    
    Increased cma memory utilization will improve overall memory
    utilization because free cma pages are ignored when memory reclaim
    is done with gfp mask GFP_KERNEL.
    
    Since most memory reclaim is done by kswapd, which uses a gfp mask
    of GFP_KERNEL, by increasing cma memory utilization we are therefore
    ensuring that less aggressive memory reclaim takes place.
    
    Increased cma memory utilization will improve performance,
    for example it will increase app concurrency.
    
    Change-Id: I809589a25c6abca51f1c963f118adfc78e955cf9
    Signed-off-by: Liam Mark <lmark@codeaurora.org>

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -55,7 +55,7 @@ enum {
 	 */
 	MIGRATE_CMA,
 #endif
-	MIGRATE_PCPTYPES,	/* the number of types on the pcp lists */
+	MIGRATE_PCPTYPES, /* the number of types on the pcp lists */
 	MIGRATE_HIGHATOMIC = MIGRATE_PCPTYPES,
 #ifdef CONFIG_MEMORY_ISOLATION
 	MIGRATE_ISOLATE,	/* can't allocate from here */
@@ -74,9 +74,11 @@ extern int *get_migratetype_fallbacks(int mtype);
 #ifdef CONFIG_CMA
 bool is_cma_pageblock(struct page *page);
 #  define is_migrate_cma(migratetype) unlikely((migratetype) == MIGRATE_CMA)
+#  define get_cma_migrate_type() MIGRATE_CMA
 #else
 #  define is_cma_pageblock(page) false
 #  define is_migrate_cma(migratetype) false
+#  define get_cma_migrate_type() MIGRATE_MOVABLE
 #endif
 
 #define for_each_migratetype_order(order, type) \
