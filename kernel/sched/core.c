6b3cb3e90b6eb8f033b0445c6d6ba6d9c5270ddb
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2014-07-30 04:51:28 -0700

    sched: window-stats: update task demand on tick
    A task can execute on a cpu for a long time without being preempted
    or migrated. In such case, its demand can become outdated for a long
    time. Prevent that from happening by updating demand of currently
    running task during scheduler tick.
    
    Change-Id: I321917b4590635c0a612560e3a1baf1e6921e792
    CRs-Fixed: 698662
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>
    [joonwoop@codeaurora.org: fixed trivial merge conflict in core.c]
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3971,7 +3971,7 @@ void scheduler_tick(void)
 	curr->sched_class->task_tick(rq, curr, 0);
 	update_cpu_load_active(rq);
 	calc_global_load_tick(rq);
-	sched_freq_tick(cpu);
+	update_task_ravg(rq->curr, rq, TASK_UPDATE, sched_clock(), NULL);
 	raw_spin_unlock(&rq->lock);
 
 	perf_event_task_tick();
