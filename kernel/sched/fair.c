12bcecd5958bc5f1f3b46f9a94895afb9ef52ec2
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2014-08-07 17:31:08 +0530

    sched: initialize env->flags variable to 0
    env->flags and env->new_dst_cpu fields are not initialized in
    load_balance() function. As a result, load_balance() could wrongly see
    LBF_SOME_PINNED flag set and access (bogus) new_dst_cpu's runqueue
    leading to invalid memory reference. Fix this by initializing
    env->flags field to 0. While we are at it, fix similar issue in
    active_load_balance_cpu_stop() function, although there is no harm
    present currently in that function with uninitialized env->flags
    variable.
    
    Change-Id: Ied470b0abd65bf2ecfa33fa991ba554a5393f649
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8963,6 +8963,7 @@ static int load_balance(int this_cpu, struct rq *this_rq,
 		.fbq_type	= all,
 		.tasks		= LIST_HEAD_INIT(env.tasks),
 		.imbalance	= 0,
+		.flags		= 0,
 	};
 
 	/*
@@ -9414,6 +9415,7 @@ static int active_load_balance_cpu_stop(void *data)
 		.src_cpu	= busiest_rq->cpu,
 		.src_rq		= busiest_rq,
 		.idle		= CPU_IDLE,
+		.flags		= 0,
 	};
 
 	raw_spin_lock_irq(&busiest_rq->lock);
