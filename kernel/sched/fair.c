a77c3551fa2b9df07958ff4d14661fb86c9ef26d
Olav Haugan <ohaugan@codeaurora.org>
2014-10-07 17:27:44 -0700

    sched: Add checks for frequency change
    We need to check for frequency change when a task is migrated due to
    affinity change and during active balance.
    
    Change-Id: I96676db04d34b5b91edd83431c236a1c28166985
    Signed-off-by: Olav Haugan <ohaugan@codeaurora.org>
    [rameezmustafa@codeaurora.org]: Port to msm-3.18]
    Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org
    [joonwoop@codeaurora.org: fixed minor conflict in core.c]
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9444,6 +9444,7 @@ static int active_load_balance_cpu_stop(void *data)
 		.flags		= 0,
 		.loop		= 0,
 	};
+	bool moved = false;
 
 	raw_spin_lock_irq(&busiest_rq->lock);
 
@@ -9474,6 +9475,7 @@ static int active_load_balance_cpu_stop(void *data)
 					cpu_online(target_cpu)) {
 			detach_task(push_task, &env);
 			push_task_detached = 1;
+			moved = true;
 		}
 		goto out_unlock;
 	}
@@ -9493,13 +9495,10 @@ static int active_load_balance_cpu_stop(void *data)
 		p = detach_one_task(&env);
 		if (p) {
 			schedstat_inc(sd, alb_pushed);
-			/*
-			 * We want to potentially lower env.src_cpu's OPP.
-			 */
-			update_capacity_of(env.src_cpu);
-		}
-		else
+			moved = true;
+		} else {
 			schedstat_inc(sd, alb_failed);
+		}
 	}
 	rcu_read_unlock();
 out_unlock:
@@ -9523,6 +9522,11 @@ out_unlock:
 		attach_one_task(target_rq, p);
 
 	local_irq_enable();
+
+	if (moved && !same_freq_domain(busiest_cpu, target_cpu)) {
+		check_for_freq_change(busiest_rq);
+		check_for_freq_change(target_rq);
+	}
 
 	if (per_cpu(dbs_boost_needed, target_cpu)) {
 		struct migration_notify_data mnd;
