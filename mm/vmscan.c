c3625097edf925a804021cd3d6353b5b2d4de4bf
Liam Mark <lmark@codeaurora.org>
2014-11-25 14:43:42 -0800

    mm: vmscan: support complete shrinker reclaim
    Ensure that shrinkers are given the option to completely drop
    their caches even when their caches are smaller than the batch size.
    
    This change helps improve memory headroom by ensuring that under
    significant memory pressure shrinkers can drop all of their caches.
    
    This change only attempts to more aggressively call the shrinkers
    during background memory reclaim, inorder to avoid hurting the
    perforamnce of direct memory reclaim.
    
    Change-Id: I8dbc29c054add639e4810e36fd2c8a063e5c52f3
    Signed-off-by: Liam Mark <lmark@codeaurora.org>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -327,6 +327,10 @@ static unsigned long do_shrink_slab(struct shrink_control *shrinkctl,
 	int nid = shrinkctl->nid;
 	long batch_size = shrinker->batch ? shrinker->batch
 					  : SHRINK_BATCH;
+	long min_cache_size = batch_size;
+
+	if (current_is_kswapd())
+		min_cache_size = 0;
 
 	freeable = shrinker->count_objects(shrinker, shrinkctl);
 	if (freeable == 0)
@@ -392,7 +396,7 @@ static unsigned long do_shrink_slab(struct shrink_control *shrinkctl,
 	 * scanning at high prio and therefore should try to reclaim as much as
 	 * possible.
 	 */
-	while (total_scan >= batch_size ||
+	while (total_scan > min_cache_size ||
 	       total_scan >= freeable) {
 		unsigned long ret;
 		unsigned long nr_to_scan = min(batch_size, total_scan);
