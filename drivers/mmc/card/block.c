da8297855a09f532425530db3a931c297fee49d8
Subhash Jadavani <subhashj@codeaurora.org>
2014-12-04 15:16:17 +0200

    mmc: block: reduce the block timeout to 30 secs
    After completion of block write request, MMC block driver waits for the
    card to come out of the programming state. If card doesn't come out of
    the programming state in 10 mins, it would be considered as error
    condition and card would reinitialized. But this 10 mins is just too huge
    and if card is continuously stuck in programming state for 10 mins,
    mmcqd thread would take increase the CPU load significantly as we are
    continuously polling for the card status (by sending status commands).
    
    This patch reduces this timeout from 10 mins to 30 secs which is quite
    reasonable for all well-behaved cards.
    
    Change-Id: I4e8eaf29c836a81419220f312ee867b0dd5cccc7
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>
    [merez@codeaurora.org: fix trivial conflicts]
    Signed-off-by: Maya Erez <merez@codeaurora.org>

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -80,7 +80,7 @@ MODULE_ALIAS("mmc:block");
 #define INAND_CMD38_ARG_SECTRIM1 0x81
 #define INAND_CMD38_ARG_SECTRIM2 0x88
 
-#define MMC_BLK_TIMEOUT_MS  (10 * 60 * 1000)        /* 10 minute timeout */
+#define MMC_BLK_TIMEOUT_MS  (30 * 1000)        /* 30 sec timeout */
 #define SD_BLK_TIMEOUT_MS  (1 * 30 * 1000)     /* for SD 30s timeout */
 
 #define MMC_SANITIZE_REQ_TIMEOUT 240000
