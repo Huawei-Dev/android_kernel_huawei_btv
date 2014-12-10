9c7eecb833cb4935bec4db94538fb5be6e91c7db
Jeff Bernard <jbernard@codeaurora.org>
2014-12-09 22:09:48 -0500

    thermal-core: Add a lock to tz_notify_trip()
    Lock the sensor threshold list during notifications so it
    isn't updated until notifications are sent out nor can
    something else update thresholds until all notifications
    are sent out.
    
    Change-Id: Iafce75fa7fceda3de867044861e4a2e3eef47c95
    Signed-off-by: Jeff Bernard <jbernard@codeaurora.org>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -434,7 +434,7 @@ static void sensor_update_work(struct work_struct *work)
 	mutex_unlock(&sensor->lock);
 }
 
-/* May be called in an interrupt context.
+/* Should not be called in an interrupt context.
  * Do NOT call sensor_set_trip from this function
  */
 int thermal_sensor_trip(struct thermal_zone_device *tz,
@@ -449,6 +449,8 @@ int thermal_sensor_trip(struct thermal_zone_device *tz,
 
 	if (list_empty(&tz->sensor.threshold_list))
 		return 0;
+
+	mutex_lock(&tz->sensor.lock);
 
 	list_for_each_entry_safe(pos, var, &tz->sensor.threshold_list, list) {
 		if ((pos->trip != trip) || (!pos->active))
@@ -467,6 +469,8 @@ int thermal_sensor_trip(struct thermal_zone_device *tz,
 			pos->notify(trip, temp, pos->data);
 		}
 	}
+
+	mutex_unlock(&tz->sensor.lock);
 
 	schedule_work(&tz->sensor.work);
 
