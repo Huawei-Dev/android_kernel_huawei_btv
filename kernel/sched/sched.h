5f9d6994229fd41ed7bf9d9a476fef02d5ae88be
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2015-02-16 16:42:59 +0530

    sched: Fix bug in average nr_running and nr_iowait calculation
    sched_get_nr_running_avg() returns average nr_running and nr_iowait
    task count since it was last invoked. Fix several bugs in their
    calculation.
    
    * sched_update_nr_prod() needs to consider that nr_running count can
      change by more than 1 when CFS_BANDWIDTH feature is used
    
    * sched_get_nr_running_avg() needs to sum up nr_iowait count across
      all cpus, rather than just one
    
    * sched_get_nr_running_avg() could race with sched_update_nr_prod(),
      as a result of which it could use curr_time which is behind a cpu's
      'last_time' value. That would lead to erroneous calculation of
      average nr_running or nr_iowait.
    
    While at it, fix also a bug in BUG_ON() check in
    sched_update_nr_prod() function and remove unnecessary nr_running
    argument to sched_update_nr_prod() function.
    
    Change-Id: I46737614737292fae0d7204c4648fb9b862f65b2
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>
    [rameezmustafa@codeaurora.org: Port to msm-3.18]
    Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1638,7 +1638,7 @@ static inline void add_nr_running(struct rq *rq, unsigned count)
 {
 	unsigned prev_nr = rq->nr_running;
 
-	sched_update_nr_prod(cpu_of(rq), rq->nr_running, true);
+	sched_update_nr_prod(cpu_of(rq), count, true);
 	rq->nr_running = prev_nr + count;
 
 	if (prev_nr < 2 && rq->nr_running >= 2) {
@@ -1665,7 +1665,7 @@ static inline void add_nr_running(struct rq *rq, unsigned count)
 
 static inline void sub_nr_running(struct rq *rq, unsigned count)
 {
-	sched_update_nr_prod(cpu_of(rq), rq->nr_running, false);
+	sched_update_nr_prod(cpu_of(rq), count, false);
 	rq->nr_running -= count;
 }
 
