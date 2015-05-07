94dcd8176509552e3eaa48aa4ae2c5aacd67284d
Adrian Hunter <adrian.hunter@intel.com>
2015-05-07 13:10:16 +0300

    mmc: core: Hold re-tuning during erase commands
    Hold re-tuning during erase commands to prevent
    it from conflicting with the sequence of commands.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -2223,6 +2223,8 @@ static int mmc_do_erase(struct mmc_card *card, unsigned int from,
 	nr = to - from + 1;
 	trace_mmc_blk_erase_start(arg, fr, nr);
 
+	mmc_retune_hold(card->host);
+
 	/*
 	 * qty is used to calculate the erase timeout which depends on how many
 	 * erase groups (or allocation units in SD terminology) are affected.
@@ -2326,7 +2328,7 @@ static int mmc_do_erase(struct mmc_card *card, unsigned int from,
 	} while (!(cmd.resp[0] & R1_READY_FOR_DATA) ||
 		 (R1_CURRENT_STATE(cmd.resp[0]) == R1_STATE_PRG));
 out:
-
+	mmc_retune_release(card->host);
 	trace_mmc_blk_erase_end(arg, fr, nr);
 	return err;
 }
