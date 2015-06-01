508588f8abb04140178fdfe81064d288221c1dea
Wang Nan <wangnan0@huawei.com>
2015-06-01 07:37:48 +0000

    tools: Move tools/perf/util/include/linux/{list.h,poison.h} to tools/include
    This patch moves list.h from tools/perf/util/include/linux/list.h to
    tools/include/linux/list.h to enable other libraries use macros in it,
    like libbpf which will be introduced by further patches. Since list.h
    depend on poison.h, poison.h is also moved.
    
    Both file use relative path, so one '..' is removed for each header to
    make them suit for new directory.
    
    MANIFEST is also updated for 'make perf-*-src-pkg'.
    
    Signed-off-by: Wang Nan <wangnan0@huawei.com>
    Cc: Alexei Starovoitov <alexei.starovoitov@gmail.com>
    Cc: Brendan Gregg <brendan.d.gregg@gmail.com>
    Cc: Daniel Borkmann <daniel@iogearbox.net>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: He Kuang <hekuang@huawei.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Kaixu Xia <xiakaixu@huawei.com>
    Cc: Masami Hiramatsu <masami.hiramatsu.pt@hitachi.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Zefan Li <lizefan@huawei.com>
    Cc: pi3orama@163.com
    Link: http://lkml.kernel.org/r/1433144296-74992-3-git-send-email-wangnan0@huawei.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/include/linux/list.h
+++ tools/include/linux/list.h
@@ -1,11 +1,17 @@
 #include <linux/kernel.h>
 #include <linux/types.h>
 
-#include "../../../../include/linux/list.h"
+#include "../../../include/linux/list.h"
 
-#ifndef PERF_LIST_H
-#define PERF_LIST_H
-
+#ifndef TOOLS_LIST_H
+#define TOOLS_LIST_H
+/**
+ * list_del_range - deletes range of entries from list.
+ * @begin: first element in the range to delete from the list.
+ * @end: last element in the range to delete from the list.
+ * Note: list_empty on the range of entries does not return true after this,
+ * the entries is in an undefined state.
+ */
 static inline void list_del_range(struct list_head *begin,
 				  struct list_head *end)
 {
