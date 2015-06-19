04479a89b216b8916d8ad8191d9c61ae54dffa2c
Miklos Szeredi <mszeredi@suse.cz>
2015-06-19 10:29:13 +0200

    vfs: add file_path() helper
    Turn
    	d_path(&file->f_path, ...);
    into
    	file_path(file, ...);
    
    Signed-off-by: Miklos Szeredi <mszeredi@suse.cz>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- drivers/block/loop.c
+++ drivers/block/loop.c
@@ -592,14 +592,14 @@ static ssize_t loop_attr_backing_file_show(struct loop_device *lo, char *buf)
 
 	spin_lock_irq(&lo->lo_lock);
 	if (lo->lo_backing_file)
-		p = d_path(&lo->lo_backing_file->f_path, buf, PAGE_SIZE - 1); /*lint !e747 */
+		p = file_path(lo->lo_backing_file, buf, PAGE_SIZE - 1);
 	spin_unlock_irq(&lo->lo_lock);
 
 	if (IS_ERR_OR_NULL(p))
 		ret = PTR_ERR(p);
 	else {
-		ret = strlen(p); /*lint !e713 !e668 */
-		memmove(buf, p, ret); /*lint !e732 */
+		ret = strlen(p);
+		memmove(buf, p, ret);
 		buf[ret++] = '\n';
 		buf[ret] = 0;
 	}
