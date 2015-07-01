fd8f584081bda4d77efcb6524dac02aa12d467b8
Dolev Raviv <draviv@codeaurora.org>
2015-07-01 12:52:25 +0300

    block: blk-flush: Add support for Barrier flag
    A barrier request is used to control ordering of write requests without
    clearing the device's cache. LLD support for barrier is optional. If LLD
    doesn't support barrier, flush will be issued instead to insure logical
    correctness.
    To maintain this fallback flush s/w path and flags are appended.
    This patch implements the necessary requests marking in order to support
    the barrier feature in the block layer.
    
    This patch implements two major changes required for the barrier support.
    (1) A new flush execution-policy is added to support "ordered" requests
        and a fallback , in case barrier is not supported by LLD.
    (2) If there is a flush pending in the flush-queue, the received barrier
        is ignored, in order not to miss a demand for an actual flush.
    
    Change-Id: I6072d759e5c3bd983105852d81732e949da3d448
    Signed-off-by: Dolev Raviv <draviv@codeaurora.org>

--- block/blk-settings.c
+++ block/blk-settings.c
@@ -832,6 +832,30 @@ void blk_queue_update_dma_alignment(struct request_queue *q, int mask)
 }
 EXPORT_SYMBOL(blk_queue_update_dma_alignment);
 
+/**
+ * blk_queue_flush - configure queue's cache flush capability
+ * @q:		the request queue for the device
+ * @flush:	0, REQ_FLUSH or REQ_FLUSH | REQ_FUA | REQ_BARRIER
+ *
+ * Tell block layer cache flush capability of @q.  If it supports
+ * flushing, REQ_FLUSH should be set.  If it supports bypassing
+ * write cache for individual writes, REQ_FUA should be set. If cache
+ * barrier is supported set REQ_BARRIER.
+ */
+void blk_queue_flush(struct request_queue *q, unsigned int flush)
+{
+	WARN_ON_ONCE(flush & ~(REQ_FLUSH | REQ_FUA | REQ_BARRIER));
+
+	if (WARN_ON_ONCE(!(flush & REQ_FLUSH) && ((flush & REQ_FUA) ||
+			(flush & REQ_BARRIER)))) {
+		flush &= ~REQ_FUA;
+		flush &= ~REQ_BARRIER;
+	}
+
+	q->flush_flags = flush & (REQ_FLUSH | REQ_FUA | REQ_BARRIER);
+}
+EXPORT_SYMBOL_GPL(blk_queue_flush);
+
 void blk_queue_flush_queueable(struct request_queue *q, bool queueable)
 {
 	spin_lock_irq(q->queue_lock);
@@ -842,19 +866,6 @@ void blk_queue_flush_queueable(struct request_queue *q, bool queueable)
 	spin_unlock_irq(q->queue_lock);
 }
 EXPORT_SYMBOL_GPL(blk_queue_flush_queueable);
-
-/**
- * blk_set_queue_depth - tell the block layer about the device queue depth
- * @q:		the request queue for the device
- * @depth:		queue depth
- *
- */
-void blk_set_queue_depth(struct request_queue *q, unsigned int depth)
-{
-	q->queue_depth = depth;
-	wbt_set_queue_depth(q->rq_wb, depth);
-}
-EXPORT_SYMBOL(blk_set_queue_depth);
 
 /**
  * blk_queue_write_cache - configure queue's write cache
