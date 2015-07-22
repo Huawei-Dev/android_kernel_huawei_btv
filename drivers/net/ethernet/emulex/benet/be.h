05d81e5e922bf9cc74e654d2f203a735f97fe92d
Sriharsha Basavapatna <sriharsha.basavapatna@avagotech.com>
2015-07-22 11:15:12 +0530

    be2net: support ndo_get_phys_port_id()
    Add be_get_phys_port_id() function to report physical port id. The port id
    should be unique across different be2net devices in the system. We use the
    chip serial number along with the physical port number for this.
    
    Signed-off-by: Sriharsha Basavapatna <sriharsha.basavapatna@avagotech.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/emulex/benet/be.h
+++ drivers/net/ethernet/emulex/benet/be.h
@@ -103,6 +103,8 @@
 
 #define MAX_VFS			30 /* Max VFs supported by BE3 FW */
 #define FW_VER_LEN		32
+#define	CNTL_SERIAL_NUM_WORDS	8  /* Controller serial number words */
+#define	CNTL_SERIAL_NUM_WORD_SZ	(sizeof(u16)) /* Byte-sz of serial num word */
 
 #define	RSS_INDIR_TABLE_LEN	128
 #define RSS_HASH_KEY_LEN	40
@@ -575,6 +577,9 @@ struct be_adapter {
 	int be_get_temp_freq;
 	u8 pf_number;
 	struct rss_info rss_info;
+	/* Filters for packets that need to be sent to BMC */
+	u32 bmc_filt_mask;
+	u16 serial_num[CNTL_SERIAL_NUM_WORDS];
 };
 
 #define be_physfn(adapter)		(!adapter->virtfn)
