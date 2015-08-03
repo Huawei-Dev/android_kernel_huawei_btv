b3e96afa7d10b5ea6acfbeb8413142dfec8b2d9d
Viresh Kumar <viresh.kumar@linaro.org>
2015-08-03 08:36:20 +0530

    cpufreq: drop !cpufreq_driver check from cpufreq_parse_governor()
    Driver is guaranteed to be present on a call to cpufreq_parse_governor()
    and there is no need to check for !cpufreq_driver. Drop it.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -576,9 +576,6 @@ static int cpufreq_parse_governor(char *str_governor, unsigned int *policy,
 {
 	int err = -EINVAL;
 
-	if (!cpufreq_driver)
-		goto out;
-
 	if (cpufreq_driver->setpolicy) {
 		if (!strncasecmp(str_governor, "performance", CPUFREQ_NAME_LEN)) {
 			*policy = CPUFREQ_POLICY_PERFORMANCE;
@@ -613,7 +610,6 @@ static int cpufreq_parse_governor(char *str_governor, unsigned int *policy,
 
 		mutex_unlock(&cpufreq_governor_mutex);
 	}
-out:
 	return err;
 }
 
