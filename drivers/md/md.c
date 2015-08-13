5d9a46d629f6cf6f5ba0a480da11bec8dd4f5039
Shaohua Li <shli@fb.com>
2015-08-13 14:31:56 -0700

    md: override md superblock recovery_offset for journal device
    Journal device stores data in a log structure. We need record the log
    start. Here we override md superblock recovery_offset for this purpose.
    This field of a journal device is meaningless otherwise.
    
    Signed-off-by: Shaohua Li <shli@fb.com>
    Signed-off-by: NeilBrown <neilb@suse.com>

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -1628,6 +1628,7 @@ static int super_1_validate(struct mddev *mddev, struct md_rdev *rdev)
 				return -EINVAL;
 			}
 			set_bit(Journal, &rdev->flags);
+			rdev->journal_tail = le64_to_cpu(sb->journal_tail);
 			break;
 		default:
 			rdev->saved_raid_disk = role;
@@ -1703,6 +1704,9 @@ static void super_1_sync(struct mddev *mddev, struct md_rdev *rdev)
 			sb->feature_map |=
 				cpu_to_le32(MD_FEATURE_RECOVERY_BITMAP);
 	}
+	/* Note: recovery_offset and journal_tail share space  */
+	if (test_bit(Journal, &rdev->flags))
+		sb->journal_tail = cpu_to_le64(rdev->journal_tail);
 	if (test_bit(Replacement, &rdev->flags))
 		sb->feature_map |=
 			cpu_to_le32(MD_FEATURE_REPLACEMENT);
@@ -8050,6 +8054,8 @@ static int remove_and_add_spares(struct mddev *mddev,
 		if (rdev->raid_disk >= 0)
 			continue;
 		if (test_bit(Faulty, &rdev->flags))
+			continue;
+		if (test_bit(Journal, &rdev->flags))
 			continue;
 		if (mddev->ro &&
 		    ! (rdev->saved_raid_disk >= 0 &&
