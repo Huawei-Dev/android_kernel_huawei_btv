33a4e176cb2abbd424233de4e413573bf98441ce
Subhash Jadavani <subhashj@codeaurora.org>
2015-08-13 16:58:03 -0700

    scsi: sd: add null pointer check for scsi disk pointer
    scsi disk driver probe first calls the PM runtime get before actually
    allocating the scsi disk structure and if the PM runtime status was set
    to suspended at this time then it will trigger scsi disk driver's
    resume callback. But scsi disk driver's resume callback assumes that
    scsi disk structure is always available when it is called but that's not
    true always. Fix this issue by skipping the scsi disk resume if scsi disk
    structure isn't allocated.
    
    Change-Id: Ica920b1e07976140dc29847f4dc61aac9fa402ae
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>

--- drivers/scsi/sd.c
+++ drivers/scsi/sd.c
@@ -3308,8 +3308,8 @@ static int sd_resume(struct device *dev)
 {
 	struct scsi_disk *sdkp = dev_get_drvdata(dev);
 
-	if (!sdkp)	/* E.g.: runtime resume at the start of sd_probe() */
-		return 0;
+	if (!sdkp)
+		return 0;	/* this can happen */
 
 	if (!sdkp->device->manage_start_stop)
 		return 0;
