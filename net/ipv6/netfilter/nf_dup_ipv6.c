9d1c3053c8d6087bd1e60e8221cc5ff2edfd10ae
Pablo Neira Ayuso <pablo@netfilter.org>
2015-08-21 21:28:10 +0200

    netfilter: nf_dup: fix sparse warnings
    >> net/ipv4/netfilter/nft_dup_ipv4.c:29:37: sparse: incorrect type in initializer (different base types)
       net/ipv4/netfilter/nft_dup_ipv4.c:29:37:    expected restricted __be32 [user type] s_addr
       net/ipv4/netfilter/nft_dup_ipv4.c:29:37:    got unsigned int [unsigned] <noident>
    
    >> net/ipv6/netfilter/nf_dup_ipv6.c:48:23: sparse: incorrect type in assignment (different base types)
       net/ipv6/netfilter/nf_dup_ipv6.c:48:23:    expected restricted __be32 [addressable] [assigned] [usertype] flowlabel
       net/ipv6/netfilter/nf_dup_ipv6.c:48:23:    got int
    
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- net/ipv6/netfilter/nf_dup_ipv6.c
+++ net/ipv6/netfilter/nf_dup_ipv6.c
@@ -31,8 +31,8 @@ static bool nf_dup_ipv6_route(struct net *net, struct sk_buff *skb,
 		fl6.flowi6_oif = oif;
 
 	fl6.daddr = *gw;
-	fl6.flowlabel = ((iph->flow_lbl[0] & 0xF) << 16) |
-			 (iph->flow_lbl[1] << 8) | iph->flow_lbl[2];
+	fl6.flowlabel = (__force __be32)(((iph->flow_lbl[0] & 0xF) << 16) |
+			(iph->flow_lbl[1] << 8) | iph->flow_lbl[2]);
 	dst = ip6_route_output(net, NULL, &fl6);
 	if (dst->error) {
 		dst_release(dst);
