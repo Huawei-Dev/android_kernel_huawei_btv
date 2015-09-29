677913e073ea383884792f74453415434206858f
Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
2015-09-29 18:21:19 +0900

    usb: renesas_usbhs: Add support for R-Car H3
    This patch adds a compatible string to support for R-Car H3.
    
    Since the HS-USB controller of R-Car H3 is almost the same specification
    with R-Car Gen2 (these have 16 pipes and usb-dmac), this patch
    sets the "type" of renesas_usbhs_driver_param to USBHS_TYPE_RCAR_GEN2.
    
    Acked-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/renesas_usbhs/common.c
+++ drivers/usb/renesas_usbhs/common.c
@@ -472,6 +472,15 @@ static const struct of_device_id usbhs_of_match[] = {
 		.compatible = "renesas,usbhs-r8a7791",
 		.data = (void *)USBHS_TYPE_RCAR_GEN2,
 	},
+	{
+		.compatible = "renesas,usbhs-r8a7794",
+		.data = (void *)USBHS_TYPE_RCAR_GEN2,
+	},
+	{
+		/* Gen3 is compatible with Gen2 */
+		.compatible = "renesas,usbhs-r8a7795",
+		.data = (void *)USBHS_TYPE_RCAR_GEN2,
+	},
 	{ },
 };
 MODULE_DEVICE_TABLE(of, usbhs_of_match);
