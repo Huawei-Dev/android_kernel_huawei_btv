457655c788dda27f43c867836f26ac1f345773a0
Eric Dumazet <edumazet@google.com>
2015-10-01 05:39:26 -0700

    inet: fix race in reqsk_queue_unlink()
    reqsk_timer_handler() tests if icsk_accept_queue.listen_opt
    is NULL at its beginning.
    
    By the time it calls inet_csk_reqsk_queue_drop() and
    reqsk_queue_unlink(), listener might have been closed and
    inet_csk_listen_stop() had called reqsk_queue_yank_acceptq()
    which sets icsk_accept_queue.listen_opt to NULL
    
    We therefore need to correctly check listen_opt being NULL
    after holding syn_wait_lock for proper synchronization.
    
    Fixes: fa76ce7328b2 ("inet: get rid of central tcp/dccp listener timer")
    Fixes: b357a364c57c ("inet: fix possible panic in reqsk_queue_unlink()")
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Cc: Yuchung Cheng <ycheng@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/inet_connection_sock.c
+++ net/ipv4/inet_connection_sock.c
@@ -522,16 +522,23 @@ EXPORT_SYMBOL(inet_rtx_syn_ack);
 static bool reqsk_queue_unlink(struct request_sock_queue *queue,
 			       struct request_sock *req)
 {
-	struct inet_hashinfo *hashinfo = req_to_sk(req)->sk_prot->h.hashinfo;
-	spinlock_t *lock;
-	bool found;
+	struct request_sock **prev;
+	struct listen_sock *lopt;
+	bool found = false;
 
-	lock = inet_ehash_lockp(hashinfo, req->rsk_hash);
-
-	spin_lock(lock);
-	found = __sk_nulls_del_node_init_rcu(req_to_sk(req));
-	spin_unlock(lock);
-
+	spin_lock(&queue->syn_wait_lock);
+	lopt = queue->listen_opt;
+	if (lopt) {
+		for (prev = &lopt->syn_table[req->rsk_hash]; *prev != NULL;
+		     prev = &(*prev)->dl_next) {
+			if (*prev == req) {
+				*prev = req->dl_next;
+				found = true;
+				break;
+			}
+		}
+	}
+	spin_unlock(&queue->syn_wait_lock);
 	if (timer_pending(&req->rsk_timer) && del_timer_sync(&req->rsk_timer))
 		reqsk_put(req);
 	return found;
