0a2b0a595d16cdb1f0e9e341f2bb2f3ec9e822e3
Darrick J. Wong <darrick.wong@oracle.com>
2015-10-17 16:16:04 -0400

    ext4: call out CRC and corruption errors with specific error codes
    Instead of overloading EIO for CRC errors and corrupt structures,
    return the same error codes that XFS returns for the same issues.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- fs/ext4/symlink.c
+++ fs/ext4/symlink.c
@@ -60,7 +60,7 @@ static const char *ext4_follow_link(struct dentry *dentry, void **cookie)
 	     sizeof(struct ext4_encrypted_symlink_data) - 1) >
 	    max_size) {
 		/* Symlink data on the disk is corrupted */
-		res = -EIO;
+		res = -EFSCORRUPTED;
 		goto errout;
 	}
 	plen = (cstr.len < EXT4_FNAME_CRYPTO_DIGEST_SIZE*2) ?
