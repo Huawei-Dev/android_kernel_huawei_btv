816c427532762fa2f0a90b9fa26342620eded489
Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
2015-11-05 18:50:48 -0800

    mm/kasan: don't use kasan shadow pointer in generic functions
    We can't use generic functions like print_hex_dump to access kasan shadow
    region.  This require us to setup another kasan shadow region for the
    address passed (kasan shadow address).  Some architectures won't be able
    to do that.  Hence make a copy of the shadow region row and pass that to
    generic functions.
    
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
    Reviewed-by: Andrey Ryabinin <ryabinin.a.a@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/kasan/report.c
+++ mm/kasan/report.c
@@ -188,10 +188,12 @@ static void print_shadow_for_address(const void *addr)
 		 * function, because generic functions may try to
 		 * access kasan mapping for the passed address.
 		 */
+		kasan_disable_current();
 		memcpy(shadow_buf, shadow_row, SHADOW_BYTES_PER_ROW);
 		print_hex_dump(KERN_ERR, buffer,
 			DUMP_PREFIX_NONE, SHADOW_BYTES_PER_ROW, 1,
 			shadow_buf, SHADOW_BYTES_PER_ROW, 0);
+		kasan_enable_current();
 
 		if (row_is_guilty(shadow_row, shadow))
 			pr_err("%*c\n",
