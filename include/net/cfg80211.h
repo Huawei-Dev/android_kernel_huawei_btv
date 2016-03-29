39029e2622eb1184669775539cdcb3ff35c153e9
Jouni Malinen <jouni@qca.qualcomm.com>
2016-03-29 13:53:27 +0300

    cfg80211: Add option to specify previous BSSID for Connect command
    This extends NL80211_CMD_CONNECT to allow the NL80211_ATTR_PREV_BSSID
    attribute to be used similarly to way this was already allowed with
    NL80211_CMD_ASSOCIATE. This allows user space to request reassociation
    (instead of association) when already connected to an AP. This provides
    an option to reassociate within an ESS without having to disconnect and
    associate with the AP.
    
    Signed-off-by: Jouni Malinen <jouni@qca.qualcomm.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Git-commit: ba6fbacf9c073effaedf0c52fe7e52e2baf67725
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/jberg/mac80211-next.git
    Change-Id: Idfd211db838cdde40ecc02a1803eac5c1ebfbc77
    CRs-fixed: 1004073
    Signed-off-by: Vidyullatha Kanchanapally <vidyullatha@codeaurora.org>

--- include/net/cfg80211.h
+++ include/net/cfg80211.h
@@ -1878,6 +1878,7 @@ struct cfg80211_ibss_params {
  * @vht_capa_mask: The bits of vht_capa which are to be used.
  * @pbss: if set, connect to a PCP instead of AP. Valid for DMG
  *	networks.
+ * @prev_bssid: previous BSSID, if not %NULL use reassociate frame
  */
 struct cfg80211_connect_params {
 	struct ieee80211_channel *channel;
@@ -1901,6 +1902,7 @@ struct cfg80211_connect_params {
 	struct ieee80211_vht_cap vht_capa;
 	struct ieee80211_vht_cap vht_capa_mask;
 	bool pbss;
+	const u8 *prev_bssid;
 };
 
 /**
