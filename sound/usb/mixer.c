11a587acdebc902e99a8ff344d6f665806c18fbb
Daniel Mack <daniel@zonque.org>
2016-04-08 19:52:02 +0200

    ALSA: usb-audio: allow clock source validity interrupts
    [ Upstream commit 191227d99a281333b50aaf82ab4152fbfa249c19 ]
    
    miniDSP USBStreamer UAC2 devices send clock validity changes with the
    control field set to zero. The current interrupt handler ignores all
    packets if the control field does not match the mixer element's, but
    it really should only do that in case that field is needed to
    distinguish multiple elements with the same ID.
    
    This patch implements a logic that lets notifications packets pass
    if the element ID is unique for a given device.
    
    Signed-off-by: Daniel Mack <daniel@zonque.org>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- sound/usb/mixer.c
+++ sound/usb/mixer.c
@@ -2396,6 +2396,7 @@ static void snd_usb_mixer_interrupt_v2(struct usb_mixer_interface *mixer,
 	__u8 unitid = (index >> 8) & 0xff;
 	__u8 control = (value >> 8) & 0xff;
 	__u8 channel = value & 0xff;
+	unsigned int count = 0;
 
 	if (channel >= MAX_CHANNELS) {
 		usb_audio_dbg(mixer->chip,
@@ -2404,6 +2405,12 @@ static void snd_usb_mixer_interrupt_v2(struct usb_mixer_interface *mixer,
 		return;
 	}
 
+	for (list = mixer->id_elems[unitid]; list; list = list->next_id_elem)
+		count++;
+
+	if (count == 0)
+		return;
+
 	for (list = mixer->id_elems[unitid]; list; list = list->next_id_elem) {
 		struct usb_mixer_elem_info *info;
 
@@ -2411,7 +2418,7 @@ static void snd_usb_mixer_interrupt_v2(struct usb_mixer_interface *mixer,
 			continue;
 
 		info = (struct usb_mixer_elem_info *)list;
-		if (info->control != control)
+		if (count > 1 && info->control != control)
 			continue;
 
 		switch (attribute) {
