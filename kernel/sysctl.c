258cb3cd566a84824c4c5f66f86016e27ff106cd
Joonwoo Park <joonwoop@codeaurora.org>
2016-05-25 12:35:45 -0700

    Revert "sched: add scheduling latency tracking procfs node"
    This reverts commit b40bf941f61756bcc ("sched: add scheduling latency
    tracking procfs node") as this feature is no longer used.
    
    Change-Id: I5de789b6349e6ea78ae3725af2a3ffa72b7b7f11
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -645,11 +645,6 @@ static struct ctl_table kern_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec_minmax,
 	},
-	{
-		.procname	= "sched_max_latency_us",
-		.mode		= 0644,
-		.proc_handler	= sched_max_latency_sysctl,
-	},
 #endif
 #ifdef CONFIG_SCHED_TUNE
 	{
