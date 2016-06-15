6cb87a985b953605b987617d95e73dd2923a8d13
Taniya Das <tdas@codeaurora.org>
2016-06-15 12:15:01 +0530

    clk: Add support to set custom flags with clk_set_flags
    The new API clk_set_flags could be used by hardware specific clock drivers
    or clients for specific hardware requirements. These flags could be custom
    defined as per hardware needs.
    
    Change-Id: Ia67373ee2b8934c898052c68338fa86cb16070dd
    Signed-off-by: Taniya Das <tdas@codeaurora.org>

--- include/linux/clk-provider.h
+++ include/linux/clk-provider.h
@@ -221,6 +221,8 @@ struct clk_rate_request {
  *		directory is provided as an argument.  Called with
  *		prepare_lock held.  Returns 0 on success, -EERROR otherwise.
  *
+ * @set_flags: Set custom flags which deals with hardware specifics. Returns 0
+ *	       on success, -EEROR otherwise.
  *
  * The clk_enable/clk_disable and clk_prepare/clk_unprepare pairs allow
  * implementations to split any work between atomic (enable) and sleepable
@@ -269,6 +271,7 @@ struct clk_ops {
 	int		(*set_phase)(struct clk_hw *hw, int degrees);
 	void		(*init)(struct clk_hw *hw);
 	int		(*debug_init)(struct clk_hw *hw, struct dentry *dentry);
+	int		(*set_flags)(struct clk_hw *hw, unsigned flags);
 };
 
 /**
