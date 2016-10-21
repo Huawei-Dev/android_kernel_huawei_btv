6dcd8c58beffc2efd30befd3356f2d825db5590d
Jaehoon Chung <jh80.chung@samsung.com>
2016-10-21 19:57:57 +0900

    mmc: dw_mmc-pltfm: fix the potential NULL pointer dereference
    commit 45c7a4908a307a023e237a64a3eadcafc4836493 upstream.
    
    platform_get_resource can be returned the NULL pointer.
    Then regs->start should be referred to NULL Pointer.
    devm_ioremap_resource() checks whether res is NULL or not.
    
    Signed-off-by: Jaehoon Chung <jh80.chung@samsung.com>
    Reviewed-by: Shawn Lin <shawn.lin@rock-chips.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/mmc/host/dw_mmc-pltfm.c
+++ drivers/mmc/host/dw_mmc-pltfm.c
@@ -48,18 +48,18 @@ int dw_mci_pltfm_register(struct platform_device *pdev,
 	host->irq_flags = 0;
 	host->pdata = pdev->dev.platform_data;
 
-	/* Get registers' physical base address */
-	host->phy_regs = regs->start;
 	host->regs = devm_ioremap_resource(&pdev->dev, regs);
 	if (IS_ERR(host->regs))
 		return PTR_ERR(host->regs);
 
-    /*私有IP数据初始化*/
 	if (drv_data && drv_data->init) {
 		ret = drv_data->init(host);
 		if (ret)
 			return ret;
 	}
+
+	/* Get registers' physical base address */
+	host->phy_regs = regs->start;
 
 	platform_set_drvdata(pdev, host);
 
