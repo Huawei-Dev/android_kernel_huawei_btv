e1a32fa29f98f3559634cc72de103ade7f45e0c1
Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
2017-02-27 11:27:10 -0800

    Revert "firmware_class: Add an additional path to fw_path list"
    This reverts commit 3e47c536248f ("firmware_class: Add an
    additional path to fw_path list").
    
    Adding /firmware/images entry in fw_path is introducing selinux
    denials as the access was happening under the client process
    context. Removing the entry from fw_path structure.
    
    Change-Id: I359f8defe49595a7e53558992080cb94eba42451
    Signed-off-by: Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>

--- drivers/base/firmware_class.c
+++ drivers/base/firmware_class.c
@@ -308,7 +308,6 @@ static const char * const fw_path[] = {
 	"/lib/firmware/updates",
 	"/lib/firmware/" UTS_RELEASE,
 	"/lib/firmware",
-	"/firmware/image,"
 	"/system/vendor/firmware",
 	"/product/etc/firmware"
 };
