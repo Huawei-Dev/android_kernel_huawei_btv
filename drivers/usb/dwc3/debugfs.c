2b52b772ddcf34e9d7c9e2a4d79d50395ab9ccbe
Sai Krishna Juturi <jsaikrishna@codeaurora.org>
2017-05-17 11:06:35 +0530

    usb : dwc3: Initialize kernel stack variables properly
    If kernel stack variables are not initialized properly,
    there  is a chance of kernel information disclosure.
    So, initialize kernel stack variables with null characters.
    
    CRs-fixed: 2042592
    Change-Id: I213c0e5c7f67824c2cecace276ff2f8f81599d51
    Signed-off-by: Sai Krishna Juturi <jsaikrishna@codeaurora.org>

--- drivers/usb/dwc3/debugfs.c
+++ drivers/usb/dwc3/debugfs.c
@@ -396,7 +396,7 @@ static ssize_t dwc3_mode_write(struct file *file,
 	struct dwc3		*dwc = s->private;
 	unsigned long		flags;
 	u32			mode = 0;
-	char			buf[32];
+	char			buf[32] = {};
 
 	if (copy_from_user(&buf, ubuf, min_t(size_t, sizeof(buf) - 1, count)))
 		return -EFAULT;
@@ -476,7 +476,7 @@ static ssize_t dwc3_testmode_write(struct file *file,
 	struct dwc3		*dwc = s->private;
 	unsigned long		flags;
 	u32			testmode = 0;
-	char			buf[32];
+	char			buf[32] = {};
 
 	if (copy_from_user(&buf, ubuf, min_t(size_t, sizeof(buf) - 1, count)))
 		return -EFAULT;
@@ -583,7 +583,7 @@ static ssize_t dwc3_link_state_write(struct file *file,
 	struct dwc3		*dwc = s->private;
 	unsigned long		flags;
 	enum dwc3_link_state	state = 0;
-	char			buf[32];
+	char			buf[32] = {};
 
 	if (copy_from_user(&buf, ubuf, min_t(size_t, sizeof(buf) - 1, count)))
 		return -EFAULT;
