fbaf7141dec3c69f25acb12883408026a6a45880
Huawei-Dev <surdu_petru@yahoo.com>
2020-09-04 17:44:52 +0200

    btv:usb: use DBG from this commit: RNDIS: Add Data aggregation (multi packet) support

--- drivers/usb/gadget/function/f_rndis.c
+++ drivers/usb/gadget/function/f_rndis.c
@@ -474,7 +474,7 @@ static void rndis_command_complete(struct usb_ep *ep, struct usb_request *req)
 			rndis->port.multi_pkt_xfer = 1;
 		else
 			rndis->port.multi_pkt_xfer = 0;
-		pr_info("%s: MaxTransferSize: %d : Multi_pkt_txr: %s\n",
+		DBG(cdev, "%s: MaxTransferSize: %d : Multi_pkt_txr: %s\n",
 				__func__, buf->MaxTransferSize,
 				rndis->port.multi_pkt_xfer ? "enabled" :
 							    "disabled");
