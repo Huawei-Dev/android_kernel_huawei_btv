arter97/caf_msm-5.4

arm64: mark split_pmd() with __init to avoid section mismatch warnings
split_pmd() calls early_alloc(), which is marked with __init. Mark
split_pmd() similarly. The only current caller of split_pmd() is
remap_pages(), which is already __init, so there was no real danger
here in the first place.

Change-Id: I3bbc4c66f1ced8fe772366b7e5287be5f474f314
Signed-off-by: Matt Wagantall <mattw@codeaurora.org>

arch/arm64/mm/mmu.c
@@ -468,7 +468,7 @@ static void __init map_mem(void)
 	memblock_set_current_limit(MEMBLOCK_ALLOC_ANYWHERE);
 }
 #ifdef CONFIG_FORCE_PAGES
-static noinline void split_pmd(pmd_t *pmd, unsigned long addr,
+static noinline void __init split_pmd(pmd_t *pmd, unsigned long addr,
 				unsigned long end, unsigned long pfn)
 {
 	pte_t *pte, *start_pte;

Huawei-Dev/android_kernel_huawei_btv

arch/arm64/mm/mmu.c
@@ -444,7 +444,7 @@ static void __init map_mem(void)
 	memblock_set_current_limit(MEMBLOCK_ALLOC_ANYWHERE);
 }
 #ifdef CONFIG_FORCE_PAGES
-static noinline void split_and_set_pmd(pmd_t *pmd, unsigned long addr,
+static noinline void __init split_and_set_pmd(pmd_t *pmd, unsigned long addr,
 				unsigned long end, unsigned long pfn)
 {
 	pte_t *pte, *start_pte;