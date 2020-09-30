2a51959bb9a467d648ae31610f07bd5bae1cdd11
Huawei-Dev <surdu_petru@yahoo.com>
2020-09-30 19:16:47 +0200

    btv:Revert: Pass the list of cpus with affected qos to notifier

--- kernel/power/qos.c
+++ kernel/power/qos.c
@@ -314,8 +314,7 @@ static const struct file_operations pm_qos_debug_fops = {
 	.release        = single_release,
 };
 
-static inline void pm_qos_set_value_for_cpus(struct pm_qos_constraints *c,
-		struct cpumask *cpus)
+static inline void pm_qos_set_value_for_cpus(struct pm_qos_constraints *c)
 {
 	struct pm_qos_request *req = NULL;
 	int cpu;
@@ -339,11 +338,8 @@ static inline void pm_qos_set_value_for_cpus(struct pm_qos_constraints *c,
 		}
 	}
 
-	for_each_possible_cpu(cpu) {
-		if (c->target_per_cpu[cpu] != qos_val[cpu])
-			cpumask_set_cpu(cpu, cpus);
+	for_each_possible_cpu(cpu)
 		c->target_per_cpu[cpu] = qos_val[cpu];
-	}
 }
 
 /**
@@ -364,7 +360,6 @@ int pm_qos_update_target(struct pm_qos_constraints *c,
 	unsigned long flags;
 	int prev_value, curr_value, new_value;
 	struct plist_node *node = &req->node;
-	struct cpumask cpus;
 	int ret;
 
 	spin_lock_irqsave(&pm_qos_lock, flags);
@@ -395,9 +390,8 @@ int pm_qos_update_target(struct pm_qos_constraints *c,
 	}
 
 	curr_value = pm_qos_get_value(c);
-	cpumask_clear(&cpus);
 	pm_qos_set_value(c, curr_value);
-	pm_qos_set_value_for_cpus(c, &cpus);
+	pm_qos_set_value_for_cpus(c);
 
 	spin_unlock_irqrestore(&pm_qos_lock, flags);
 
@@ -407,7 +401,7 @@ int pm_qos_update_target(struct pm_qos_constraints *c,
 		if (c->notifiers)
 			blocking_notifier_call_chain(c->notifiers,
 						     (unsigned long)curr_value,
-						     &cpus);
+						     NULL);
 	} else {
 		ret = 0;
 	}
