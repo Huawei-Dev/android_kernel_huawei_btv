758e73be3ad7e7ea8aacb2b4c7827bac789f94ae
Huawei-Dev <surdu_petru@yahoo.com>
2020-10-08 21:42:30 +0200

    btv:Revert: Fix potential race condition in rproc_add

--- drivers/remoteproc/remoteproc_core.c
+++ drivers/remoteproc/remoteproc_core.c
@@ -1398,6 +1398,11 @@ int rproc_add(struct rproc *rproc)
 	ret = device_add(dev);
 	if (ret < 0)
 		return ret;
+		
+	/* expose to rproc_get_by_phandle users */
+	mutex_lock(&rproc_list_mutex);
+	list_add(&rproc->node, &rproc_list);
+	mutex_unlock(&rproc_list_mutex);
 
 	dev_info(dev, "%s is available\n", rproc->name);
 
@@ -1406,20 +1411,11 @@ int rproc_add(struct rproc *rproc)
 
 	/* create debugfs entries */
 	rproc_create_debug_dir(rproc);
-	ret = rproc_add_virtio_devices(rproc);
-	if (ret < 0)
 #ifdef CONFIG_HISI_REMOTEPROC
 	return 0;
 #else
-	return ret;
+	return rproc_add_virtio_devices(rproc);
 #endif
-
-	/* expose to rproc_get_by_phandle users */
-	mutex_lock(&rproc_list_mutex);
-	list_add(&rproc->node, &rproc_list);
-	mutex_unlock(&rproc_list_mutex);
-
-	return 0;
 }
 EXPORT_SYMBOL(rproc_add);
 
