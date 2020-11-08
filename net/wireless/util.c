2f3c74b9ac0f878a9356f7f697b0f5386893cc1b
Huawei-Dev <surdu_petru@yahoo.com>
2020-11-08 12:02:26 +0100

    Revert: cfg80211: identically validate beacon interval for AP/MESH/IBSS

--- net/wireless/util.c
+++ net/wireless/util.c
@@ -1500,7 +1500,7 @@ int cfg80211_validate_beacon_int(struct cfg80211_registered_device *rdev,
 		.beacon_int_gcd = beacon_int,	/* GCD(n) = n */
 	};
 
-	if (beacon_int < 10 || beacon_int > 10000)
+	if (!beacon_int)
 		return -EINVAL;
 
 	params.iftype_num[iftype] = 1;
