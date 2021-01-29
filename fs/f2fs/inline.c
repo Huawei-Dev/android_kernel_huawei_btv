7fe16c6dc47f6f0dd9f9c9b6e61c24910880888d
Huawei-Dev <surdu_petru@yahoo.com>
2021-01-29 23:38:47 +0100

    btv: fix commit: ANDROID: Refactor fs readpage/write tracepoints.

--- fs/f2fs/inline.c
+++ fs/f2fs/inline.c
@@ -98,12 +98,16 @@ int f2fs_read_inline_data(struct inode *inode, struct page *page)
 
 	ipage = f2fs_get_node_page(F2FS_I_SB(inode), inode->i_ino);
 	if (IS_ERR(ipage)) {
+		trace_android_fs_dataread_end(inode, page_offset(page),
+					      PAGE_SIZE);
 		unlock_page(page);
 		return PTR_ERR(ipage);
 	}
 
 	if (!f2fs_has_inline_data(inode)) {
 		f2fs_put_page(ipage, 1);
+		trace_android_fs_dataread_end(inode, page_offset(page),
+					      PAGE_SIZE);
 		return -EAGAIN;
 	}
 
@@ -115,6 +119,8 @@ int f2fs_read_inline_data(struct inode *inode, struct page *page)
 	if (!PageUptodate(page))
 		SetPageUptodate(page);
 	f2fs_put_page(ipage, 1);
+	trace_android_fs_dataread_end(inode, page_offset(page),
+				      PAGE_SIZE);
 	unlock_page(page);
 	return 0;
 }
