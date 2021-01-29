dce18de5c4f6d549bd151a3e16f74a503b848242
Huawei-Dev <surdu_petru@yahoo.com>
2021-01-29 23:25:49 +0100

    btv:f2fs: fix format typo

--- fs/f2fs/dir.c
+++ fs/f2fs/dir.c
@@ -816,7 +816,7 @@ int f2fs_fill_dentries(struct dir_context *ctx, struct f2fs_dentry_ptr *d,
 		if (unlikely(bit_pos > d->max ||
 				le16_to_cpu(de->name_len) > F2FS_NAME_LEN)) {
 			f2fs_warn(sbi, "%s: corrupted namelen=%d, run fsck to fix.",
-				__func__, le16_to_cpu(de->name_len));
+				  __func__, le16_to_cpu(de->name_len));
 			set_sbi_flag(sbi, SBI_NEED_FSCK);
 			err = -EFSCORRUPTED;
 			goto out;
